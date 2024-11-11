bec1df9756ae4d198775be2d640988d9541cef44
Anatoli <tolik.petrosyants@gmail.com>
2024-11-11 15:01:47 +0400

    feat: Add xcode project

---
+++ Spinpick/Spinpick/SpinpickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinpickApp.swift
+//  Spinpick
+//
+//  Created by Anatoli Petrosyants on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SpinpickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
