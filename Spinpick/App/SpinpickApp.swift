fdb2189c5884ac40b604df4990d74dbaaadf6016
Anatoli <tolik.petrosyants@gmail.com>
2024-11-11 15:09:34 +0400

    feat: Add folders structure

---
+++ Spinpick/App/SpinpickApp.swift
@@ -0,0 +1,23 @@
+//
+//  SpinpickApp.swift
+//  Spinpick
+//
+//  Created by Anatoli Petrosyants on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SpinpickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
